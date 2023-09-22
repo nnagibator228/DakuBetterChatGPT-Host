cd56b2599e17e865b327788ff7e0b970d5fbc82f
arteom <plzd@ntcry.ru>
2023-09-22 21:57:55 +0300

    cloudflare adapter & configurations added

---
+++ svelte.config.js
@@ -0,0 +1,11 @@
+import adapter from '@sveltejs/adapter-cloudflare';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter(),
+    // ... truncated ...
+  }
+};
+
+export default config;
